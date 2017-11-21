8ad59c0971307781ebb5b45659b3b546de226e72
Damir Drempetic <ddrempe@gmail.com>
2017-11-21 22:44:05 +0100

    Added main menu and symmetric encryption class

---
+++ SeeSharpUtilities/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* SOURCES:
+https://stackoverflow.com/questions/20365214/a-simple-menu-in-a-console-application
+*/
+
+namespace SeeSharpUtilities
+{
+    class Menu
+    {
+        public string menuName { get; set; }
+
+        public List<string> menuOptions { get; set; }
+
+        public Menu(string name, List<string> options)
+        {
+            menuName = name;
+            menuOptions = options;
+        }
+
+        public int Display()
+        {
+            Console.WriteLine(menuName);
+            Console.WriteLine();
+            Console.WriteLine("0. Exit");
+            foreach (string option in menuOptions)
+            {
+                Console.WriteLine(option);
+            }
+
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+    }
+}
---
+++ SeeSharpUtilities/RunableOptions.cs
@@ -0,0 +1,37 @@
+﻿using Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharpUtilities
+{
+    class RunableOptions
+    {
+        public static void Cryptography()
+        {
+            List<string> options = new List<string>
+            {
+                "1. Symmetric encryption of a string",
+                "2. Symmetric decryption of a string"
+            };
+            Menu cryptographyMenu = new Menu("Cryptography", options);
+
+            int userInput = 0;
+            do
+            {
+                userInput = cryptographyMenu.Display();
+
+                switch (userInput)
+                {
+                    case 1:
+                        //SymmetricEncryption.Encrypt();
+                        break;
+                    default:
+                        break;
+                }
+            } while (userInput != 0);
+        }
+    }
+}
